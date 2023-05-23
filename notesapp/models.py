195e0ff9cc8339af9995ae9ffc187b36b9e36ccd
pozdrowieniadowiezienia <soother.impetus_07@icloud.com>
2023-05-23 13:36:29 +0200

    Added Note model

--- notesapp/models.py
+++ notesapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Note(models.Model):
+    PRIORITY_CHOICES = (
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    )
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='low')
+
+    class Meta:
+        ordering = ['-priority', '-created_date']
